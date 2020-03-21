0ebcdc6862fe3d2d4b23bb9e812e58560704d625
Micah Dutro <mdutrolaw@gmail.com>
2020-03-21 01:18:34 -0500

    Add "Day" to WeatherCards to clarify Martian day number

--- client/src/WeatherCards.js
+++ client/src/WeatherCards.js
@@ -6,7 +6,7 @@ const WeatherCards = (props) => {
   return (
     <Card style={{ width: '8rem' }}>
       <Card.Body>
-        <Card.Title>{props.sol}</Card.Title>
+        <Card.Title>Day {props.sol}</Card.Title>
         {/* Take a slice of the UTC format date string to get 3 digit month, 2 digit day format */}
         <Card.Subtitle className="mb-2 text-muted">{props.date.slice(4, 10)}</Card.Subtitle>
         <Card.Text>
