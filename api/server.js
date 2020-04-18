0b1f55e7bad1c6b0490a46c37c53cab8853a0cb3
MDutro <mdutrolaw@gmail.com>
2020-04-18 17:09:36 -0500

    Add template literal Mars URL

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const local = process.env.LOCAL_KEY;
 app.use(cors());
 
 app.get('/', (req, res) => {
-  const marsUrl = 'https://api.nasa.gov/insight_weather/?api_key=' + mars + '&feedtype=json&ver=1.0';
+  const marsUrl = `https://api.nasa.gov/insight_weather/?api_key=${mars}&feedtype=json&ver=1.0`;
   axios.get(marsUrl)
   .then(response => response.data)
   .then(response => res.send(response))
